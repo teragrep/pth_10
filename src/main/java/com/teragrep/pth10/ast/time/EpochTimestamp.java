a166bc06e385927465e7a80d004e9b9b92eea632
eemhu <125959687+eemhu@users.noreply.github.com>
2024-11-12 10:08:45 +0200

    added new variable unquotedValue for epochFromString, changed equals("") check to isEmpty()

--- src/main/java/com/teragrep/pth10/ast/time/EpochTimestamp.java
+++ src/main/java/com/teragrep/pth10/ast/time/EpochTimestamp.java
@@ -1,3 +1,48 @@
+/*
+ * Teragrep Data Processing Language (DPL) translator for Apache Spark (pth_10)
+ * Copyright (C) 2019-2024 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
 package com.teragrep.pth10.ast.time;
 
 import com.teragrep.pth10.ast.DPLTimeFormat;
@@ -10,6 +55,7 @@ import java.text.ParseException;
 import java.util.Objects;
 
 public final class EpochTimestamp {
+
     private final String value;
     private final String timeformat;
 
@@ -33,22 +79,22 @@ public final class EpochTimestamp {
 
     // Uses defaultTimeFormat if timeformat is null and DPLTimeFormat if timeformat isn't null (which means that the
     // timeformat= option was used).
-    private long epochFromString(String value, String timeFormatString) {
-        value = new UnquotedText(new TextString(value)).read(); // erase the possible outer quotes
-        long timevalue = 0;
-        if (timeFormatString == null || timeFormatString.equals("")) {
-            timevalue = new DefaultTimeFormat().getEpoch(value);
+    private long epochFromString(final String value, final String timeFormatString) {
+        final String unquotedValue = new UnquotedText(new TextString(value)).read(); // erase the possible outer quotes
+        final long timevalue;
+        if (timeFormatString == null || timeFormatString.isEmpty()) {
+            timevalue = new DefaultTimeFormat().getEpoch(unquotedValue);
         }
         else {
             // TODO: should be included in DPLTimeFormat
             if (timeFormatString.equals("%s")) {
-                return Long.parseLong(value);
+                return Long.parseLong(unquotedValue);
             }
             try {
-                timevalue = new DPLTimeFormat(timeFormatString).getEpoch(value);
+                timevalue = new DPLTimeFormat(timeFormatString).getEpoch(unquotedValue);
             }
             catch (ParseException e) {
-                throw new RuntimeException("TimeQualifier conversion error: <" + value + "> can't be parsed.");
+                throw new RuntimeException("TimeQualifier conversion error: <" + unquotedValue + "> can't be parsed.");
             }
         }
         return timevalue;
@@ -56,8 +102,10 @@ public final class EpochTimestamp {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         EpochTimestamp that = (EpochTimestamp) o;
         return Objects.equals(value, that.value) && Objects.equals(timeformat, that.timeformat);
     }
