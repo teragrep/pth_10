6ce5255ee252960f81fa1891284f40da5cd9784b
eemhu <125959687+eemhu@users.noreply.github.com>
2025-11-11 15:17:58 +0200

    Issue 647: Bloom create and update now returns only status messages. (#693)
    * add CustomDataset object & test, set bloom create and update to return status dataset instead of the whole dataset.
    
    * apply spotless
    
    * add isStreaming(boolean) option for CustomDataset.
    
    * separate makeRowsList and makeRowsSeq into Rows object & tests.
    
    * change Bloom filter result to contain _time column and also switch result column to _raw.
    
    * remove null sparksession from CustomDatasetTest.testEqualsContract
    
    * remove double call to collectAsList() in CustomDatasetTest.testCreateStreamingDataset
    
    * add CustomDataset interface, rename CustomDataset -> CustomDatasetImpl, remove isStreaming ctor arg, this is now handled by CustomResultStep object instead. Result dataset is now created in a separate CustomResultStep, which is added after bloom create and update.
    
    * removed unused variables from TeragrepBloomStep
    
    * remove possibly unnecessary DataStreamWriter from CustomDatasetImpl

---
+++ src/main/java/com/teragrep/pth_10/datasources/CustomDatasetImpl.java
@@ -0,0 +1,105 @@
+/*
+ * Teragrep Data Processing Language (DPL) translator for Apache Spark (pth_10)
+ * Copyright (C) 2019-2025 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+package com.teragrep.pth_10.datasources;
+
+import com.teragrep.pth_10.ast.DPLParserCatalystContext;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder;
+import org.apache.spark.sql.catalyst.encoders.RowEncoder;
+import org.apache.spark.sql.execution.streaming.MemoryStream;
+import org.apache.spark.sql.types.StructType;
+import scala.Option;
+
+import java.util.List;
+import java.util.Objects;
+
+public final class CustomDatasetImpl implements CustomDataset {
+
+    private final StructType schema;
+    private final List<Object[]> values;
+    private final DPLParserCatalystContext catCtx;
+
+    public CustomDatasetImpl(
+            final StructType schema,
+            final List<Object[]> values,
+            final DPLParserCatalystContext catCtx
+    ) {
+        this.schema = schema;
+        this.values = values;
+        this.catCtx = catCtx;
+    }
+
+    @Override
+    public Dataset<Row> asStaticDataset() {
+        return catCtx.getSparkSession().createDataFrame(new Rows(values).asList(), schema);
+    }
+
+    @Override
+    public Dataset<Row> asStreamingDataset() {
+        final SQLContext sqlContext = catCtx.getSparkSession().sqlContext();
+        final ExpressionEncoder<Row> encoder = RowEncoder.apply(schema);
+        final MemoryStream<Row> rowMemoryStream = new MemoryStream<>(1, sqlContext, Option.apply(1), encoder);
+
+        rowMemoryStream.addData(new Rows(values).asSeq());
+
+        return rowMemoryStream.toDF();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final CustomDatasetImpl that = (CustomDatasetImpl) o;
+        return Objects.equals(schema, that.schema) && Objects.equals(values, that.values)
+                && Objects.equals(catCtx, that.catCtx);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(schema, values, catCtx);
+    }
+}
---
+++ src/test/java/com/teragrep/pth_10/CustomDatasetImplTest.java
@@ -0,0 +1,139 @@
+/*
+ * Teragrep Data Processing Language (DPL) translator for Apache Spark (pth_10)
+ * Copyright (C) 2019-2025 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+package com.teragrep.pth_10;
+
+import com.teragrep.pth_10.ast.DPLParserCatalystContext;
+import com.teragrep.pth_10.datasources.CustomDatasetImpl;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.OutputMode;
+import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.types.*;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public final class CustomDatasetImplTest {
+
+    private StreamingTestUtil streamingTestUtil;
+
+    @BeforeAll
+    void setEnv() {
+        this.streamingTestUtil = new StreamingTestUtil();
+        this.streamingTestUtil.setEnv();
+    }
+
+    @BeforeEach
+    void setUp() {
+        this.streamingTestUtil.setUp();
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.streamingTestUtil.tearDown();
+    }
+
+    @Test
+    void testEqualsContract() {
+        final SparkSession sparkSession = streamingTestUtil.getCtx().getSparkSession();
+        EqualsVerifier
+                .forClass(CustomDatasetImpl.class)
+                .withPrefabValues(DPLParserCatalystContext.class, new DPLParserCatalystContext(sparkSession), new DPLParserCatalystContext(sparkSession)).verify();
+    }
+
+    @Test
+    void testCreateStreamingDataset() {
+        final CustomDatasetImpl customDataset = new CustomDatasetImpl(new StructType(new StructField[] {
+                StructField.apply("fieldName", DataTypes.StringType, false, new MetadataBuilder().build())
+        }), Arrays.asList(new Object[] {
+                "row1"
+        }, new Object[] {
+                "row2"
+        }), streamingTestUtil.getCtx());
+
+        final Dataset<Row> resultDs = Assertions.assertDoesNotThrow(customDataset::asStreamingDataset);
+        Assertions.assertTrue(resultDs.isStreaming());
+        final List<Dataset<Row>> listOfBatches = new ArrayList<>();
+        final StreamingQuery sq = Assertions.assertDoesNotThrow(() -> {
+            return resultDs.writeStream().format("memory").outputMode(OutputMode.Append()).foreachBatch((ds, id) -> {
+                listOfBatches.add(ds);
+            }).start();
+        });
+        sq.processAllAvailable();
+        Assertions.assertDoesNotThrow(sq::stop);
+        Assertions.assertDoesNotThrow(() -> sq.awaitTermination());
+
+        Assertions.assertEquals(1, listOfBatches.size());
+        Assertions.assertEquals(2, listOfBatches.get(0).count());
+        final List<Row> collected = listOfBatches.get(0).collectAsList();
+        Assertions.assertEquals("row1", collected.get(0).getString(0));
+        Assertions.assertEquals("row2", collected.get(1).getString(0));
+    }
+
+    @Test
+    void testCreateStaticDataset() {
+        final CustomDatasetImpl customDataset = new CustomDatasetImpl(new StructType(new StructField[] {
+                StructField.apply("fieldName", DataTypes.StringType, false, new MetadataBuilder().build())
+        }), Arrays.asList(new Object[] {
+                "row1"
+        }, new Object[] {
+                "row2"
+        }), streamingTestUtil.getCtx());
+
+        final Dataset<Row> resultDs = Assertions.assertDoesNotThrow(customDataset::asStaticDataset);
+        Assertions.assertFalse(resultDs.isStreaming());
+
+        Assertions.assertEquals(2, resultDs.count());
+        final List<Row> collected = resultDs.collectAsList();
+        Assertions.assertEquals("row1", collected.get(0).getString(0));
+        Assertions.assertEquals("row2", collected.get(1).getString(0));
+    }
+}
